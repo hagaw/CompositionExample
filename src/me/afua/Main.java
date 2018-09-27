958303dc515f21f701fe463c0245b9c8477a2b7f
smewled <smewled2012@gmail.com>
2018-09-27 11:31:01 -0400

    melak

--- src/me/afua/Main.java
+++ src/me/afua/Main.java
@@ -1,49 +1,53 @@
 package me.afua;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Person theParent = new Person();
-        theParent.setName("Afua");
+        Scanner names = new Scanner(System.in);
+        int parentCounter;
 
-//        Adding children here
-        Person childOne = new Person();
-        childOne.setName("First Child");
-        theParent.addAChild(childOne);
+        do {
+            parentCounter = 0;
+            System.out.println("Is there any Parent to register in my list? ");
+            String nextParent = names.nextLine();
+            if ((nextParent).equalsIgnoreCase("yes")) {
+            System.out.println("Hello Parent! What is you name?");
+            String theparent = names.nextLine();
+            Person parent = new Person();
+            parent.setName(theparent);
 
-        Person childTwo = new Person();
-        childTwo.setName("Second Child");
-        theParent.addAChild(childTwo);
+                System.out.println(theparent + "\t How many children you have? ");
+                int childNumber = names.nextInt();
+                names.nextLine();
+                for(int childCounter =1; childCounter <=childNumber; childCounter++){
+                    System.out.println("please write your child's name ");
+                    String theChild = names.nextLine();
+                    Person child = new Person();
+                    child.setName(theChild);
+                    parent.addAChild(child);
+                }
+            ArrayList<String> theirNames = new ArrayList<>();
+            for (Person eachChild :parent.getMyChildren()) {
 
-        Person childThree = new Person();
-        childThree.setName("Third Child");
-        theParent.addAChild(childThree);
+                theirNames.add(eachChild.getName());
+                System.out.println(parent.getName() + "'s child is " + eachChild.getName());
+            }
+                parentCounter =1;
+                Collections.sort(theirNames);
+                System.out.println("Mr./Mrs. " +theparent+" Your children are listed below in alphabetic order :\n");
+                for(String aName:theirNames) {
+                    System.out.println(aName);
+                }
+            }
+        } while (parentCounter == 1);
 
-        ArrayList <String> theirNames  = new ArrayList<>();
-        for(Person eachChild: theParent.getMyChildren())
-        {
-
-            theirNames.add(eachChild.getName());
-            System.out.println(theParent.getName()+"'s child is "+eachChild.getName());
-        }
-
-        Collections.sort(theirNames);
-        System.out.println("Sorted Names:\n");
-        for(String aName:theirNames)
-        {
-            System.out.println(aName);
-        }
-
-
-
-
-
-
-
+        System.out.println("Thank you, Good Bye !!!");
     }
 }
+
+
+
